0123ec9b0d34cb7f74d8f53b3f9ed78a152d3254
James Deromedi <jd1504a@student.american.edu>
2019-01-27 18:59:42 -0500

    added figures

--- Homework 1/FamilyofODEs.m
+++ Homework 1/FamilyofODEs.m
@@ -1,9 +1,9 @@
-a = 1;
-b = 10;
+a = 10;
+b = 0;
 tspan = 0:20;
 
-for i = -b: b
-    [t,x] = ode45(@(t,x) -1*power(x,3) + a*x - i, tspan, linspace(-2,2,4));
+for i = 0: a
+    [t,x] = ode45(@(t,x) -1*power(x,3) + i*x - b, tspan, linspace(-2,2,4));
     hold on;
     plot(t,x);
     hold off;
